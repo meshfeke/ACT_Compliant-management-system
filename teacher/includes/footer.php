e6aee16e1b2a15df394be654c79b9a712ec8ee33
Shimelis Fekadu <mesh.feke@gmail.com>
2023-04-23 04:04:40 +0300

    added Notify with SMS page

--- teacher/includes/footer.php
+++
@@ -1,8 +0,0 @@
- <footer class="site-footer">
-          <div class="text-center">
-              DBMS Mini project
-              <a href="#" class="go-top">
-                  <i class="fa fa-angle-up"></i>
-              </a>
-          </div>
-      </footer>
