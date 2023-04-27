1d11739c8b0519ff8f6b7c30bf36adf281778481
Shimelis Fekadu <mesh.feke@gmail.com>
2023-04-27 16:11:01 +0300

    some files in js updated

--- js/myLogic.js
+++ js/myLogic.js
@@ -2,8 +2,7 @@ var keycloak = new Keycloak();
 
 function initKeycloak() {
     keycloak.init({onLoad: 'login-required'}).then(function() {
-        constructTableRows(keycloak.idTokenParsed);
-        pasteToken(keycloak.token);
+
     }).catch(function() {
         alert('failed to initialize');
     });
